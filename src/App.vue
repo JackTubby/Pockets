2a9f0b088edb4f845065134e94ffc4e0e9eda41f
JackTubby <jack200034@hotmail.com>
2023-08-28 22:00:08 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+  import { ref } from 'vue'
+
+  export default {
+    setup() {
+      const pocket = ref({
+        title: null,
+        description: null,
+        totalValue: null,
+      })
+      return {
+        pocket,
+      }
+    },
+  }
+</script>
+
+<template>
+  <main class="container mx-auto">
+    <div class="text-center">
+      <h1>Pockets</h1>
+    </div>
+  </main>
+</template>

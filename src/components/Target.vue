ac03300dd8e5834932cb4bbde19059d99e69ee46
JackTubby <jack200034@hotmail.com>
2023-09-03 21:45:26 +0100

    Init

---
+++ src/components/Target.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="p-4">
+    <div class="flex">
+      <p class="w-1/3">£0</p>
+      <div class="w-1/3"></div>
+      <p class="w-1/3">£1000</p>
+    </div>
+  </div>
+</template>

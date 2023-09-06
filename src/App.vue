3170064e258df68540ffddd01f696cc7813a8a77
JackTubby <jack200034@hotmail.com>
2023-09-06 22:18:02 +0100

    Organise &  add modal

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,36 @@
-<script lang="ts">
-  import { ref, defineComponent } from 'vue'
-  import Pockets from './components/Pockets.vue'
-
-  interface Pocket {
-    title: string | null,
-    description: string | null,
-    totalValue: number | null,
-  }
-
-  export default defineComponent({
-    components: {
-      Pockets,
-    },
-    setup() {
-      const pocket = ref<Pocket>({
-        title: null,
-        description: null,
-        totalValue: null,
-      })
-      
-      return {
-        pocket,
-      }
-    },
-  })
-</script>
-
 <template>
   <main class="container mx-auto">
-    <div class="text-center">
+    <div class="text-center flex flex-col gap-y-4">
       <h1>Pockets</h1>
+      <div>
+        <button @click="openCreateModal" class="bg-green-400 px-6 py-2 rounded-2xl">Add Pocket</button>
+      </div>
     </div>
     <div>
       <Pockets />
     </div>
+    <Modal :show="showModal" @close="showModal = false" >
+        
+    </Modal>
   </main>
 </template>
+<script lang="ts">
+  import { defineComponent, ref } from 'vue'
+  import Pockets from './components/Pockets.vue'
+  import Modal from './components/Modal.vue'
+
+  export default defineComponent({
+    components: {
+      Pockets,
+      Modal,
+    },
+    setup() {
+      const showModal = ref(false)
+      const openCreateModal = () => {
+        showModal.value = true
+        console.log('Opening Modal')
+      }
+      return { openCreateModal, showModal }
+    }
+  })
+</script>

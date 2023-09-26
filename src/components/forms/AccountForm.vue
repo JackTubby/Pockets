2cce15502d3e284801d932cd0da8c73c933909a7
JackTubby <jack200034@hotmail.com>
2023-09-26 21:49:03 +0100

    Handle form data

--- src/components/forms/AccountForm.vue
+++ src/components/forms/AccountForm.vue
@@ -1,17 +1,33 @@
 <template>
-  <div class="flex flex-col items-center gap-y-4">
+  <form class="flex flex-col items-center gap-y-4" @submit.prevent="handleSubmit">
     <select class="w-8/12 p-2 text-lg border border-gray-300 rounded-md" name="" id="" placeholder="Select Bank">
-      <option value="test">Test</option>
+      <option value="test">NatWest</option>
+      <option value="test">Nation Wide</option>
     </select>
-    <input class="w-8/12 p-2 text-lg border border-gray-300 rounded-md" type="text" placeholder="Card Name">
-    <input class="w-8/12 p-2 text-lg border border-gray-300 rounded-md" type="text" placeholder="Last Four Digits">
-    <input class="w-8/12 p-2 text-lg border border-gray-300 rounded-md" type="number" placeholder="Balance">
+    <input class="w-8/12 p-2 text-lg border border-gray-300 rounded-md" type="text" placeholder="Card Name" v-model="formData.name">
+    <input class="w-8/12 p-2 text-lg border border-gray-300 rounded-md" type="text" placeholder="Last Four Digits" v-model="formData.digits">
+    <input class="w-8/12 p-2 text-lg border border-gray-300 rounded-md" type="number" placeholder="Balance" v-model="formData.balance">
     <button class="bg-green-500 text-white font-semibold py-2 px-6 rounded-md text-xl hover:bg-green-400 shadow-md" type="submit">Create Pocket</button>
-  </div>
+  </form>
 </template>
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
+  setup() {
+    const formData = ref({
+      name: '',
+      digits: '',
+      balance: '',
+    })
+    // Handle form submission
+    const handleSubmit = () => {
+      console.log(formData.value)
+    }
+    return {
+      formData,
+      handleSubmit
+    }
+  }
 });
 </script>

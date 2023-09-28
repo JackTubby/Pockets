d587ea1de9196319afd7f47bf9b8db22876f3096
Jack Tubby <jack200034@hotmail.com>
2023-09-28 21:45:41 +0100

    Format

--- src/components/Pockets.vue
+++ src/components/Pockets.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class=" w-1/4">
-    <div class="border-2 rounded-2xl flex flex-col gap-y-10 min-h-[16rem] shadow-2xl">
+  <div class="w-1/4">
+    <div
+      class="border-2 rounded-2xl flex flex-col gap-y-10 min-h-[16rem] shadow-2xl"
+    >
       <!-- Header -->
-      <div class="flex justify-between text-center pt-4 px-4">
-        <h3 class="text-xl font-semiBold w-1/3">Pocket 1</h3>
+      <div class="flex justify-between px-4 pt-4 text-center">
+        <h3 class="w-1/3 text-xl font-semiBold">Pocket 1</h3>
         <h5 class="w-1/3">£100</h5>
         <i class="w-1/3">ICON</i>
       </div>
@@ -14,7 +16,7 @@
             <div class="bg-green-500 w-[3px] absolute h-6"></div>
           </div>
           <li class="flex text-center">
-            <div class="font-bold w-1/3">£100</div>
+            <div class="w-1/3 font-bold">£100</div>
             <div class="w-1/3">27/05/23</div>
             <div class="w-1/3">£100</div>
           </li>
@@ -26,12 +28,12 @@
   </div>
 </template>
 <script lang="ts">
-  import { defineComponent} from 'vue'
-  import Target from './Target.vue'
+import { defineComponent } from "vue";
+import Target from "./Target.vue";
 
-  export default defineComponent({
-    components: {
-      Target,
-    }
-  })
+export default defineComponent({
+  components: {
+    Target,
+  },
+});
 </script>

a8ea41efc7c38b1135ff51eeb5640a316cd8fc9e
Jack Tubby <jack200034@hotmail.com>
2023-09-28 21:46:02 +0100

    Format

--- src/components/forms/AccountForm.vue
+++ src/components/forms/AccountForm.vue
@@ -1,25 +1,50 @@
 <template>
   <div>
-    <form class="flex flex-col items-center gap-y-4" @submit.prevent="handleSubmit">
-      <select class="w-8/12 p-2 text-lg border border-gray-300 rounded-md" name="" id="" placeholder="Select Bank"
-        v-model="formData.bank">
+    <form
+      class="flex flex-col items-center gap-y-4"
+      @submit.prevent="handleSubmit"
+    >
+      <select
+        class="w-8/12 p-2 text-lg border border-gray-300 rounded-md"
+        name=""
+        id=""
+        placeholder="Select Bank"
+        v-model="formData.bank"
+      >
         <option value="NatWest">NatWest</option>
         <option value="NationWide">Nation Wide</option>
       </select>
-      <input class="w-8/12 p-2 text-lg border border-gray-300 rounded-md" type="text" placeholder="Card Name"
-        v-model="formData.name">
-      <input class="w-8/12 p-2 text-lg border border-gray-300 rounded-md" type="text" placeholder="Last Four Digits"
-        v-model="formData.digits">
-      <input class="w-8/12 p-2 text-lg border border-gray-300 rounded-md" type="number" placeholder="Balance"
-        v-model="formData.balance">
-      <button class="px-6 py-2 text-xl font-semibold text-white bg-green-500 rounded-md shadow-md hover:bg-green-400"
-        type="submit">Create Pocket</button>
+      <input
+        class="w-8/12 p-2 text-lg border border-gray-300 rounded-md"
+        type="text"
+        placeholder="Card Name"
+        v-model="formData.name"
+      />
+      <input
+        class="w-8/12 p-2 text-lg border border-gray-300 rounded-md"
+        type="text"
+        placeholder="Last Four Digits"
+        v-model="formData.digits"
+      />
+      <input
+        class="w-8/12 p-2 text-lg border border-gray-300 rounded-md"
+        type="number"
+        placeholder="Balance"
+        v-model="formData.balance"
+      />
+      <button
+        class="px-6 py-2 text-xl font-semibold text-white bg-green-500 rounded-md shadow-md hover:bg-green-400"
+        type="submit"
+      >
+        Add Account
+      </button>
     </form>
     <div>
       <h2>Saved Pockets:</h2>
       <ul>
         <li v-for="(savedData, index) in savedFormData" :key="index">
-          {{ savedData.name }} - {{ savedData.bank }} - {{ savedData.digits }} - {{ savedData.balance }}
+          {{ savedData.name }} - {{ savedData.bank }} - {{ savedData.digits }} -
+          {{ savedData.balance }}
         </li>
       </ul>
     </div>
@@ -27,7 +52,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, onMounted } from 'vue';
+import { defineComponent, ref, onMounted } from "vue";
 
 interface FormData {
   bank: string;
@@ -39,10 +64,10 @@ interface FormData {
 export default defineComponent({
   setup() {
     const formData = ref<FormData>({
-      bank: '',
-      name: '',
-      digits: '',
-      balance: '',
+      bank: "",
+      name: "",
+      digits: "",
+      balance: "",
     });
 
     const savedFormData = ref<FormData[]>([]);
@@ -52,17 +77,17 @@ export default defineComponent({
       // Save the current form data to local storage
       const currentData = { ...formData.value };
       savedFormData.value.push(currentData);
-      localStorage.setItem('formData', JSON.stringify(savedFormData.value));
+      localStorage.setItem("formData", JSON.stringify(savedFormData.value));
       formData.value = {
-        bank: '',
-        name: '',
-        digits: '',
-        balance: '',
+        bank: "",
+        name: "",
+        digits: "",
+        balance: "",
       };
     };
     // Load saved data from local storage on component mount
     const loadDataFromLocalStorage = () => {
-      const storedData = localStorage.getItem('formData');
+      const storedData = localStorage.getItem("formData");
       if (storedData) {
         savedFormData.value = JSON.parse(storedData);
       }

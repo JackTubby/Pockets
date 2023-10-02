89546a1ebe7897f8b874646f01fceae958a13a7d
JackTubby <jack200034@hotmail.com>
2023-10-02 21:38:35 +0100

    Create transfer.vue

---
+++ src/components/transfers/Transfer.vue
@@ -0,0 +1,21 @@
+<template>
+  <Modal :show="show" @close="$emit('close')">
+    <h1>Transfer</h1>
+  </Modal>
+</template>
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import Modal from "../modals/Modal.vue";
+
+export default defineComponent({
+  components: {
+    Modal,
+  },
+  props: {
+    show: Boolean,
+    type: String,
+  },
+  emits: ["close"],
+  setup(){}
+});
+</script>

92c14148065052bf7abdf2e96cef2740b4778de4
JackTubby <jack200034@hotmail.com>
2023-10-02 21:58:19 +0100

    Update accounts styling

--- src/components/Accounts.vue
+++ src/components/Accounts.vue
@@ -1,10 +1,17 @@
 <template>
-  <div>
-    <button @click="run" class="p-2 text-white bg-green-500 font-xl">
-      HERE
-    </button>
-    <div>
-      <p></p>
+  <div class="container w-full">
+    <div class="w-1/4 border-2 border-primary rounded-lg px-4 py-8 flex gap-y-6 flex-col">
+      <div>
+        <h2 class="text-left">NatWest</h2>
+      </div>
+      <div class="flex justify-center items-end gap-x-2">
+        <p class="text-3xl">£1,242.93</p>
+        <p class="text-sm">GBP</p>
+      </div>
+      <div class="flex">
+        <p class="w-1/2 break-words text-sm">****************6472</p>
+        <p class="w-1/2 text-sm">MR JACK TUBBY</p>
+      </div>
     </div>
   </div>
 </template>

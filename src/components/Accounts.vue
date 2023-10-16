a2a8cbb1107dc1a5a1a5f3a128993cbe3f98ecf5
JackTubby <jack200034@hotmail.com>
2023-10-16 23:04:35 +0100

    Accept props

--- src/components/Accounts.vue
+++ src/components/Accounts.vue
@@ -2,15 +2,15 @@
   <div class="container w-full">
     <div class="w-1/4 border-2 border-primary rounded-lg px-4 py-8 flex gap-y-6 flex-col">
       <div>
-        <h2 class="text-left">NatWest</h2>
+        <h2 class="text-left">{{ bankName }}</h2>
       </div>
       <div class="flex justify-center items-end gap-x-2">
-        <p class="text-3xl">£1,242.93</p>
-        <p class="text-sm">GBP</p>
+        <p class="text-3xl">{{ balance }}</p>
+        <p class="text-sm">{{ currency }}</p>
       </div>
       <div class="flex">
-        <p class="w-1/2 break-words text-sm">****************6472</p>
-        <p class="w-1/2 text-sm">MR JACK TUBBY</p>
+        <p class="w-1/2 break-words text-sm">****************{{ fourDigits }}</p>
+        <p class="w-1/2 text-sm">{{ name }}</p>
       </div>
     </div>
   </div>
@@ -18,15 +18,18 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import accountHelpers from "../firebase/accountHelpers";
 
 export default defineComponent({
+  props: {
+    bankName: String,
+    balance: String,
+    currency: String,
+    fourDigits: String,
+    name: String,
+  },
   setup() {
-    const run = accountHelpers.get;
 
-    return {
-      run,
-    };
+    return {};
   },
 });
 </script>

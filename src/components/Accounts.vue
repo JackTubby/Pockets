7c9463447c6c4991ac14dd7ca6aa7931b938f7d7
JackTubby <jack200034@hotmail.com>
2023-10-29 13:47:50 +0000

    Remove container wrapper

--- src/components/Accounts.vue
+++ src/components/Accounts.vue
@@ -1,17 +1,15 @@
 <template>
-  <div class="container w-full">
-    <div class="w-1/4 border-2 border-primary rounded-lg px-4 py-8 flex gap-y-6 flex-col">
-      <div>
-        <h2 class="text-left">{{ bank }}</h2>
-      </div>
-      <div class="flex justify-center items-end gap-x-2">
-        <p class="text-3xl">{{ balance }}</p>
-        <p class="text-sm">{{ currency }}</p>
-      </div>
-      <div class="flex">
-        <p class="w-1/2 break-words text-sm">****************{{ digits }}</p>
-        <p class="w-1/2 text-sm">{{ name }}</p>
-      </div>
+  <div class="w-1/4 border-2 border-primary rounded-lg px-4 py-8 flex gap-y-6 flex-col">
+    <div>
+      <h2 class="text-left">{{ bank }}</h2>
+    </div>
+    <div class="flex justify-center items-end gap-x-2">
+      <p class="text-3xl">{{ balance }}</p>
+      <p class="text-sm">{{ currency }}</p>
+    </div>
+    <div class="flex">
+      <p class="w-1/2 break-words text-sm">****************{{ digits }}</p>
+      <p class="w-1/2 text-sm">{{ name }}</p>
     </div>
   </div>
 </template>
@@ -29,7 +27,7 @@ export default defineComponent({
   },
   setup() {
 
-    return {}; 
+    return {};
   },
 });
 </script>

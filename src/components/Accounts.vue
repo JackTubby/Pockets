5835f539983151370e7cf9e63ad6cc34485f85df
JackTubby <jack200034@hotmail.com>
2023-10-29 13:31:55 +0000

    Update prop types

--- src/components/Accounts.vue
+++ src/components/Accounts.vue
@@ -2,14 +2,14 @@
   <div class="container w-full">
     <div class="w-1/4 border-2 border-primary rounded-lg px-4 py-8 flex gap-y-6 flex-col">
       <div>
-        <h2 class="text-left">{{ bankName }}</h2>
+        <h2 class="text-left">{{ bank }}</h2>
       </div>
       <div class="flex justify-center items-end gap-x-2">
         <p class="text-3xl">{{ balance }}</p>
         <p class="text-sm">{{ currency }}</p>
       </div>
       <div class="flex">
-        <p class="w-1/2 break-words text-sm">****************{{ fourDigits }}</p>
+        <p class="w-1/2 break-words text-sm">****************{{ digits }}</p>
         <p class="w-1/2 text-sm">{{ name }}</p>
       </div>
     </div>
@@ -21,15 +21,15 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   props: {
-    bankName: String,
-    balance: String,
+    bank: String,
+    balance: Number,
     currency: String,
-    fourDigits: String,
+    digits: Number,
     name: String,
   },
   setup() {
 
-    return {};
+    return {}; 
   },
 });
 </script>

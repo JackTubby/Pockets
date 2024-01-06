fef0eaabde9439280708e566501ef27f061d20c0
Jack Tubby <jack200034@hotmail.com>
2024-01-06 23:49:22 +0000

    Update names to be more generic

--- src/components/menus/MainMenu.vue
+++ src/components/menus/MainMenu.vue
@@ -2,10 +2,10 @@
   <div class="absolute right-10 bottom-10">
     <div class="flex flex-col items-end gap-y-4">
       <button v-if="menu" class="z-20 px-6 py-2 bg-primary text-primary-content hover:bg-primary-focus rounded-2xl"
-        @click="openCreateModal('account')">Add Account</button>
+        @click="openModal('account')">Add Account</button>
 
       <button v-if="menu" class="z-20 px-6 py-2 bg-primary text-primary-content hover:bg-primary-focus rounded-2xl"
-        @click="openCreateModal('pocket')">Add Pocket</button>
+        @click="openModal('pocket')">Add Pocket</button>
 
       <div
         class="z-10 flex items-center justify-center text-4xl transition-all ease-in-out rounded-full cursor-pointer bg-success hover:opacity-50 text-primary-content w-14 h-14"
@@ -19,6 +19,6 @@
 
 <script lang="ts">
 export default {
-  props: ['menu', 'openCreateModal', 'showMenu'],
+  props: ['menu', 'openModal', 'showMenu'],
 }
 </script>

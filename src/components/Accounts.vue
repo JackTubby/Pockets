948419d13fcccf6211d450e9163f6ad5b924777f
Jack Tubby <jack200034@hotmail.com>
2024-01-06 13:29:40 +0000

    Add dropdown menu

--- src/components/Accounts.vue
+++ src/components/Accounts.vue
@@ -1,8 +1,19 @@
 <template>
   <!-- This is code for the accounts containing all the bank account information -->
-  <div class="flex flex-col w-1/4 px-4 py-8 border-2 rounded-lg border-primary gap-y-6">
-    <div>
+  <div
+    class="flex flex-col w-1/4 px-4 py-8 border-2 rounded-lg border-primary gap-y-6"
+  >
+    <div class="flex">
       <h2 class="text-left">{{ bank }}</h2>
+      <div class="ml-auto mr-5">
+        <DropDownMenu
+          :accountId="id"
+          :menu="menu"
+          :openModal="() => openModal(modalType, id)"
+          :showMenu="showMenu"
+          @close="showModal = false"
+        />
+      </div>
     </div>
     <div class="flex items-end justify-center gap-x-2">
       <p class="text-3xl">{{ balance }}</p>
@@ -13,22 +24,42 @@
       <p class="w-1/2 text-sm">{{ name }}</p>
     </div>
   </div>
+  <AlterModal :show="showModal" @close="showModal = false" :type="modalType" :accountId="accountId"></AlterModal>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
+<script setup lang="ts">
+import { ref } from "vue";
+import DropDownMenu from "./menus/DropDownMenu.vue";
+import AlterModal from "./modals/AlterModal.vue";
 
-export default defineComponent({
-  props: {
-    bank: String,
-    balance: Number,
-    currency: String,
-    digits: String,
-    name: String,
-  },
-  setup() {
-
-    return {};
-  },
+// props from app.vue from the fetch account data req
+defineProps({
+  id: String,
+  bank: String,
+  balance: Number,
+  currency: String,
+  digits: String,
+  name: String,
 });
+
+// Handle opening & closing menu
+const menu = ref(false);
+const showMenu = () => {
+  menu.value = !menu.value;
+};
+
+const showModal = ref(false); // if show or hide
+const modalType = ref(""); // what modal to show
+const accountId = ref("");// store the clicked account
+const openModal = (type: string, accountIdParam: string) => {
+  console.log(accountId.value, modalType.value, showModal.value)
+  accountId.value = accountIdParam;
+  modalType.value = type;
+  showModal.value = true;
+};
+
+// Make the dropdown menu work
+// Create the edit/delete modal
+// functionality to edit/delete - decide if I should put logic in the edit/delete modal or keep here (prob better here)
+// need to make sure when the edit/delete modal is opened the id is passed
 </script>
---
+++ src/components/menus/DropDownMenu.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="absolute ml-auto">
+    <div>
+      <div @click="showMenu">
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/2311/2311524.png"
+          width="20"
+          height="20"
+          alt="menu icon"
+          class="mb-3 ml-auto cursor-pointer img-small"
+        />
+      </div>
+      <div class="absolute">
+        <button
+          v-if="menu"
+          class="z-20 px-6 py-2 mb-4 bg-primary text-primary-content hover:bg-primary-focus rounded-2xl"
+          @click="openModal('edit', accountId)"
+        >
+          Edit Account
+        </button>
+        <button
+          v-if="menu"
+          class="z-20 px-6 py-2 bg-primary text-primary-content hover:bg-primary-focus rounded-2xl"
+          @click="openModal('delete', accountId)"
+        >
+          Delete Account
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps(["menu", "openModal", "showMenu", "accountId"]);
+</script>

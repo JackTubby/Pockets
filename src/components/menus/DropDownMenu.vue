f66927f56437e8b89f085b1c6c04fb688a820cf2
JackTubby <jack200034@hotmail.com>
2024-01-13 20:32:53 +0000

    Update styling

--- src/components/menus/DropDownMenu.vue
+++ src/components/menus/DropDownMenu.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="absolute ml-auto">
     <div>
-      <div @click="showMenu()">
+      <div v-if="!menu" @click="showMenu()">
         <img
           src="https://cdn-icons-png.flaticon.com/512/2311/2311524.png"
           width="20"
@@ -10,17 +10,16 @@
           class="mb-3 ml-auto cursor-pointer img-small"
         />
       </div>
-      <div class="absolute">
+      <img v-if="menu" @click="showMenu()" class="cursor-pointer" src="https://img.icons8.com/?size=256&amp;id=82771&amp;format=png" width="20" height="20" alt="Close icon">
+      <div v-if="menu" class="absolute bg-base-100 rounded-2xl border border-primary">
         <button
-          v-if="menu"
-          class="z-20 px-6 py-2 mb-4 bg-primary text-primary-content hover:bg-primary-focus rounded-2xl"
+          class="z-20 text-primary hover:bg-primary-focus rounded-t-2xl p-4 hover:text-secondary-content"
           @click="openModal('editAccount', accountId)"
         >
           Edit Account
         </button>
         <button
-          v-if="menu"
-          class="z-20 px-6 py-2 bg-primary text-primary-content hover:bg-primary-focus rounded-2xl"
+          class="z-20 text-primary hover:bg-primary-focus rounded-b-2xl p-4 hover:text-secondary-content"
           @click="openModal('deleteAccount', accountId)"
         >
           Delete Account

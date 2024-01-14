7d06783fd780a81f8b5097e93a3fefb506ae8abc
JackTubby <jack200034@hotmail.com>
2024-01-14 21:31:46 +0000

    init

---
+++ src/components/pockets/PocketViewOne.vue
@@ -0,0 +1,15 @@
+<template>
+  <section>
+    <div>
+      <h2>Pocket 1</h2>
+      <i>ICON</i>
+      <i>ADD</i>
+    </div>
+    <div>
+      <h3>£100</h3>
+    </div>
+    <div>
+      <!-- THE TARGET -->
+    </div>
+  </section>
+</template>
---
+++ src/components/pockets/PocketViewTwo.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    <div>
+      <h2>Pocket 1</h2>
+      <h3>£100</h3>
+      <i>ICON</i>
+    </div>
+    <div>
+      <ul>
+        <li>
+          <span>02/04/23</span>
+          <span>£100</span>
+          <span>£0</span>
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>

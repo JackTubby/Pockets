6600069c602e59de1e3a6607b6aa7bb50d8ab11b
JackTubby <jack200034@hotmail.com>
2024-01-16 22:05:19 +0000

    Change cta menu to primary

--- src/components/menus/MainMenu.vue
+++ src/components/menus/MainMenu.vue
@@ -8,7 +8,7 @@
         @click="openModal('pocket')">Add Pocket</button>
 
       <div
-        class="z-10 flex items-center justify-center text-4xl transition-all ease-in-out rounded-full cursor-pointer bg-success hover:opacity-50 text-primary-content w-14 h-14"
+        class="z-10 flex items-center justify-center text-4xl transition-all ease-in-out rounded-full cursor-pointer bg-primary hover:opacity-50 text-primary-content w-14 h-14"
         @click="showMenu()">
         <span v-if="menu">-</span>
         <span v-else>+</span>
